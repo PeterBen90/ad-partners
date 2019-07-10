581fde700d7dc9e0c3eaab4f79d1d8cf44d6e1ee
Peter Benvenuto <peterbenvenuto90@gmail.com>
2019-07-10 00:30:53 -0400

    second commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -60,10 +60,10 @@
 
 					// Images (in the format of 'url': 'alignment').
 						images: {
-							'images/slides/slide01.jpg': '0% 35%',
-							'images/slides/slide02.jpg': 'center',
-							'images/slides/slide03.jpg': 'center',
-							'images/slides/slide04.jpg': 'center'
+							'images/slides/slide01.jpeg': 'bottom',
+							'images/slides/slide02.jpeg': 'center',
+							'images/slides/slide03.jpeg': 'center',
+							'images/slides/slide04.jpeg': 'bottom'
 						},
 
 					// Delay.
